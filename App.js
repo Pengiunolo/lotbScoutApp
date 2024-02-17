c6032ac4a946ebc27d76ee338db89f58bcf98299
victor <therealpenguinolo@gmail.com>
2024-02-17 16:47:53 -0500

    added basic layout

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    
-    <View style={styles.container}>      
-
-        <Text>Open up App.js to start working on your app!</Text>
-      
+    <View style={styles.main}>
+      <View style={styles.head}>
+        <Text>test</Text>
+      </View>
+      <View style={styles.body}>      
+        <Text>Autonomus</Text>
+        <View style={styles.container}></View>
+        <Text>Teleop</Text>
+        <View style={styles.container}></View>
+        <Text>End Game</Text>
+        <View style={styles.container}></View>
+      </View>
     </View>
   );
 }
@@ -15,9 +22,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: 'center',
-
+    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#666',
+    width: '80%',
+    alignSelf: 'center'
   },
+  head:{
+    flex: 2,
+    backgroundColor: '#00ff',
+    justifyContent: 'flex-start',
+    width: '100%'
+  },
+  body:{
+    flex: 20,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  main:{
+    flex:1,
+    alignItems: 'center',
+  }
+
   
 });
