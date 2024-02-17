efe21782b5136596e6f5ff936fe16a319b9e3f6c
victor <therealpenguinolo@gmail.com>
2024-02-17 13:33:27 -0500

    added stuff to .gitignore

--- App.js
+++ App.js
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <view style={styles.topbar}>
+
+      </view>
       
+      <view style={styles.mainBody}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </view>
     </View>
   );
 }
@@ -13,8 +19,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    
+    
     justifyContent: 'center',
+  },
+  topbar:{
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#00f"
+  },
+  mainBody:{
+    flex: 5,
   },
 });
