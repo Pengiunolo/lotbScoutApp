7f72f8dea98c103700fc99cb45d4c961e4e124d9
victor <therealpenguinolo@gmail.com>
2024-02-24 15:47:55 -0500

    Made sure Ryden knows where the color Pallete is

--- db.js
+++ db.js
@@ -2,3 +2,14 @@ import { enablePromise, openDatabase } from "react-native-sqlite-storage";
 
 enablePromise(true);
 
+
+export const connectToDatabase = async () => {
+  return openDatabase(
+    { name: "yourProjectName.db", location: "default" },
+    () => {},
+    (error) => {
+      console.error(error)
+      throw Error("Could not connect to database")
+    }
+  )
+}
